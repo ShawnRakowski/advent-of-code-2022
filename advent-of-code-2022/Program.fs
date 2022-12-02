04cbab39cef6c1abd49a1469a453b81723ea13e1
Shawn Rakowski <shawn.rakowski@tcgplayer.com>
2022-12-02 06:31:57 -0600

    Day 1

---
+++ advent-of-code-2022/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+open System.IO
+
+let input = (([], 0), File.ReadAllLines("input.txt"))
+            ||> Seq.fold(fun (l, c) curr -> if String.IsNullOrWhiteSpace(curr) then ((l @ [c]), 0) else (l, c + int curr))
+            |> fun (l, c) -> l @ [c]
+
+input
+    |> Seq.max
+    |> printfn "%d"
+
+input
+    |> Seq.sortDescending
+    |> Seq.take 3
+    |> Seq.sum
+    |> printfn "%d"
