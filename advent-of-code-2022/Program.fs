9b0d2fa87995fcc8352de189d576f1f9cd30cc8c
Shawn Rakowski <srakowski@live.com>
2022-12-02 19:50:45 -0600

    day 2

--- advent-of-code-2022/Program.fs
+++ advent-of-code-2022/Program.fs
@@ -14,3 +14,56 @@ input
     |> Seq.take 3
     |> Seq.sum
     |> printfn "%d"
+
+"input2.txt"
+    |> File.ReadAllLines
+    |> Seq.map(function
+                | "A X" -> 1 + 3 // ROCK ROCK
+                | "A Y" -> 2 + 6 // ROCK PAPER
+                | "A Z" -> 3 + 0 // ROCK SCISSORS
+                | "B X" -> 1 + 0 // PAPER ROCK
+                | "B Y" -> 2 + 3 // PAPER PAPER
+                | "B Z" -> 3 + 6 // PAPER SCISSORS
+                | "C X" -> 1 + 6 // SCISSORS ROCK
+                | "C Y" -> 2 + 0 // SCISSORS PAPER
+                | "C Z" -> 3 + 3 // SCISSORS SCISSORS
+                | _ -> 0
+    ) 
+    |> Seq.sum
+    |> printfn "%d"
+
+type RpsOpts =
+    | Rock = 1
+    | Paper = 2
+    | Scissors = 3
+
+let loseTo rpsOpt =
+    match rpsOpt with
+    | RpsOpts.Rock -> RpsOpts.Scissors |> int
+    | RpsOpts.Paper -> RpsOpts.Rock |> int
+    | RpsOpts.Scissors -> RpsOpts.Paper |> int
+    | _ -> 0
+
+let winOver rpsOpt =
+    match rpsOpt with
+    | RpsOpts.Rock -> RpsOpts.Paper |> int
+    | RpsOpts.Paper -> RpsOpts.Scissors |> int
+    | RpsOpts.Scissors -> RpsOpts.Rock |> int
+    | _ -> 0
+
+"input2.txt"
+    |> File.ReadAllLines
+    |> Seq.map(function
+                | "A X" -> loseTo RpsOpts.Rock + 0      // ROCK LOSE
+                | "A Y" -> int RpsOpts.Rock + 3         // ROCK DRAW
+                | "A Z" -> winOver RpsOpts.Rock + 6     // ROCK WIN
+                | "B X" -> loseTo RpsOpts.Paper + 0     // PAPER LOSE
+                | "B Y" -> int RpsOpts.Paper + 3        // PAPER DRAW
+                | "B Z" -> winOver RpsOpts.Paper + 6    // PAPER WIN
+                | "C X" -> loseTo RpsOpts.Scissors + 0  // SCISSORS LOSE
+                | "C Y" -> int RpsOpts.Scissors + 3     // SCISSORS DRAW
+                | "C Z" -> winOver RpsOpts.Scissors + 6 // SCISSORS WIN
+                | _ -> 0
+    ) 
+    |> Seq.sum
+    |> printfn "%d"
